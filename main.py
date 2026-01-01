b137f6f6d84e80b206de3d3efcf1a131f004a4d8
Gangsu-developer <chlrkdtn2377@gmail.com>
2026-01-02 05:01:54 +0900

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-f
+from fastapi import Fastapi
+import yfinance as yf
+
+app = Fastapi()
+
+@app.get("/")
+def root() :
+    return {"status": "ok"}
+
+@app.get("/assets")
+def get_assets(ticker: str) :
+    symbols = ticker.split(",")
+    result = {}
+
+    for symbol in symbols :
+        try :
+            stock = yf.Ticker(symbol)
+            info = stock.info
+
+            result[symbol] = {
+                "symbol": symbol,
+                "name": info.get["shortName"],
+                "currency": info.get["currency"],
+                "price": info.get["currentPrice"],
+                "previous_price": info.get["previousClose"],    
+            }
+        except Exception as e:
+            result[symbol] = {
+                "symbol": symbol,
+                "error": str(e)
+            }
+    return result
